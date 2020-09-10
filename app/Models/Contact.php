82a655c144e0cbdea62c3e02acf0d3d9603bf7ed
Augusto Vinícius <augusto.geek@gmail.com>
2020-09-10 00:09:36 -0300

    add address and contact models

---
+++ app/Models/Contact.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'name', 'last_name', 'image','phone','email',
+    ];
+
+    public function address()
+    {
+        return $this->belongsTo('App\Models\Address');
+    }
+}
